cb6ee4e83d2ddd22da0f5173a4cd4476272d36cf
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-09-05 23:48:04 +0900

    First commit

---
+++ Gemfile
@@ -0,0 +1,37 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'sqlite3'
+
+# html
+gem 'haml-rails'
+
+# javascript
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'turbolinks'
+
+# CSS
+gem 'sass-rails', '~> 4.0.0'
+
+gem 'jbuilder', '~> 1.2'
+gem 'unicorn'
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+group :development do
+    gem 'erb2haml'
+end
+
+group :development, :test do
+    gem 'debugger'
+end
+
+group :doc do
+    # bundle exec rake doc:rails generates the API under doc/api.
+    gem 'sdoc', require: false
+end
+
+# 重いらしい http://d.hatena.ne.jp/suu-g/20121222/1356189597
+# gem 'therubyracer', platforms: :ruby
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gpc::Application.config.secret_key_base = 'a6da7401162e6ac2f9ca34447830c74a1d04922cccb4a4a94734ffd58f16865d720bae251299458bbde77a18827df2aa4b64915801609e0ac7d995d3f341a2ff'
